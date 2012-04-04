6e8640983170fffe5c722dd68e53b45c3fb816c9
Daniele Spidalieri <lemo_dii@hotmail.com>
2012-04-04 18:42:08 +0200

    homework1

---
+++ 2012-04-03/exercise1.js
@@ -0,0 +1,105 @@
+/**
+* @author Daniele Spidalieri 266491
+*/
+
+/**
+* Reproduce a 2D model of the floor plan of the Barcelona Pavilion
+* using only the plasm.js primitives POLYLINE and STRUCT.
+* Let assume the grid of squares on the floor be 1.0m x 1.0m.
+*/
+
+// perimetro mappa
+var externalWallsPoints = [[38.9,1],[39,1],[39,0],[0,0],[0,2],[1,2],[1,22],
+													[9,22],[9,17],[39,17],[39,16],[51,16],
+													[51,6],[52,6],[52,4],
+													[38.9,4]
+													];
+var externalWalls = POLYLINE(externalWallsPoints);
+
+//piscina grande
+var bigPoolPoints = [[1,1],[1,10],[21,10],[21,1],[1,1]];
+var bigPool= POLYLINE(bigPoolPoints);
+
+//negozio
+var shopPoints = [[5,19.2],[5,17],[1,17],[1,22],[5,22],[5,19.8]];
+var shop = POLYLINE(shopPoints);
+
+//aministrazione
+var adminPoints = [[7.2,17],[5,17],[5,22],[9,22],[9,17],[7.8,17]];
+var adminBathPoints = [[5.8,20.7],[5,20.7],[5,22],[7,22],[7,20.7],[6.6,20.7]];
+var adminToilettePoints = [[7,20.7],[7,22],[9,22],[9,20.7],[7,20.7]];
+var admin = POLYLINE(adminPoints);
+var adminBath = POLYLINE(adminBathPoints);
+var adminToilette = POLYLINE(adminToilettePoints);
+var adminRoom = STRUCT([admin,adminBath,adminToilette]);
+
+//mura interne
+var internalWall1Points = [[7.5,15],[26.5,15]];
+var internalWall1 = POLYLINE(internalWall1Points);
+var internalWall2Points = [[25.2,7.3],[33.8,7.3]];
+var internalWall2 = POLYLINE(internalWall2Points);
+var internalWall3Points = [[37.2,11.5],[42.5,11.5]];
+var internalWall3 = POLYLINE(internalWall3Points);
+var internalWall4Points = [[44.7,6.9],[44.7,14.1]];
+var internalWall4 = POLYLINE(internalWall4Points);
+var internalWall5Points = [[31,7.3],[31,13.7]];
+var internalWall5 = POLYLINE(internalWall5Points);
+var internalWall6Points = [[32,7.3],[32,13.7]];
+var internalWall6 = POLYLINE(internalWall6Points);
+var internalWall7Points = [[30,13.7],[40,13.7]];
+var internalWall7 = POLYLINE(internalWall7Points);
+var internalWalls = STRUCT([internalWall1,internalWall2,internalWall3,internalWall4,
+													internalWall5,internalWall6,internalWall7
+												 ]);
+// perimetro stanza interna
+var internalRoomPoints = [[37.7,16],[47,16],[47,5],[30,5]];
+var internalRoom = POLYLINE(internalRoomPoints);
+
+//piscina piccola
+var smallPoolPoints = [[47,5],[47,16],[51,16],[51,5],[47,5]];
+var smallPool = POLYLINE(smallPoolPoints);
+
+//colonne
+var pillar1Points = [[25.925,6.925],[25.925,7.075],[26.075,7.075],[26.075,6.925],[25.925,6.925]];
+var pillar1 = POLYLINE(pillar1Points);
+var pillar2Points = [[25.925,13.925],[25.925,14.075],[26.075,14.075],[26.075,13.925],[25.925,13.925]];
+var pillar2 = POLYLINE(pillar2Points);
+var pillar3Points = [[32.255,6.925],[32.255,7.075],[32.405,7.075],[32.405,6.925],[32.255,6.925]];
+var pillar3 = POLYLINE(pillar3Points);
+var pillar4Points = [[32.255,13.925],[32.255,14.075],[32.405,14.075],[32.405,13.925],[32.255,13.925]];
+var pillar4 = POLYLINE(pillar4Points);
+var pillar5Points = [[38.585,6.925],[38.585,7.075],[38.735,7.075],[38.735,6.925],[38.585,6.925]];
+var pillar5 = POLYLINE(pillar5Points);
+var pillar6Points = [[38.585,13.925],[38.585,14.075],[38.735,14.075],[38.735,13.925],[38.585,13.925]];
+var pillar6 = POLYLINE(pillar6Points);
+var pillar7Points = [[44.915,6.925],[44.915,7.075],[45.065,7.075],[45.065,6.925],[44.915,6.925]];
+var pillar7 = POLYLINE(pillar7Points);
+var pillar8Points = [[44.915,13.925],[44.915,14.075],[45.065,14.075],[45.065,13.925],[44.915,13.925]];
+var pillar8 = POLYLINE(pillar8Points);
+var pillars = STRUCT([pillar1,pillar2,pillar3,pillar4,pillar5,pillar6,pillar7,pillar8]);
+
+//scalinata
+var step1Points = [[36.1,1],[36.1,4],[36.5,4],[36.5,1],[36.1,1]];
+var step1 = POLYLINE(step1Points);
+var step2Points = [[36.5,1],[36.5,4],[36.9,4],[36.9,1],[36.5,1]];
+var step2 = POLYLINE(step2Points);
+var step3Points = [[36.9,1],[36.9,4],[37.3,4],[37.3,1],[36.9,1]];
+var step3 = POLYLINE(step3Points);
+var step4Points = [[37.3,1],[37.3,4],[37.7,4],[37.7,1],[37.3,1]];
+var step4 = POLYLINE(step4Points);
+var step5Points = [[37.7,1],[37.7,4],[38.1,4],[38.1,1],[37.7,1]];
+var step5 = POLYLINE(step5Points);
+var step6Points = [[38.1,1],[38.1,4],[38.5,4],[38.5,1],[38.1,1]];
+var step6 = POLYLINE(step6Points);
+var step7Points = [[38.5,1],[38.5,4],[38.9,4],[38.9,1],[38.5,1]];
+var step7 = POLYLINE(step7Points);
+var steps = STRUCT([step1,step2,step3,step4,step5,step6,step7]);
+
+//panchina
+var benchPoints = [[7.8,14.2],[7.8,14.7],[22.2,14.7],[22.2,14.2],[7.8,14.2]];
+var bench = POLYLINE(benchPoints);
+
+//piantina completa
+var plan = STRUCT([externalWalls,bigPool,shop,adminRoom,internalWalls,
+									internalRoom,smallPool,steps,pillars,bench]);
+DRAW(plan);
---
+++ 2012-04-03/exercise2.js
@@ -0,0 +1,100 @@
+/**
+* @author Daniele Spidalieri 266491
+*/
+
+/**
+* Reproduce a 3D model of the Barcellona Pavilion
+* using only the plasm.js primitives SIMPLEX_GRID and STRUCT.
+* Affine transformations T, S or R are also allowed. 
+*/
+
+// pavimentazione
+var floor1 = SIMPLEX_GRID([[39],[1],[1]]);
+var floor2 = T([1])([1])(SIMPLEX_GRID([[1],[1],[1]]));
+var floor3 = T([0,1])([21,1])(SIMPLEX_GRID([[15],[3],[1]]));
+var floor4 = T([0,1])([21,4])(SIMPLEX_GRID([[18],[13],[1]]));
+var floor5 = T([0,1])([39,4])(SIMPLEX_GRID([[8],[12],[1]]));
+var floor6 = T([0,1])([1,10])(SIMPLEX_GRID([[20],[7],[1]]));
+var floor7 = T([0,1])([1,17])(SIMPLEX_GRID([[8],[5],[1]]));
+var floor8 = T([0,1])([39,4])(SIMPLEX_GRID([[13],[1],[1]]));
+var floor9 = T([0,1])([51,5])(SIMPLEX_GRID([[1],[1],[1]]));
+var floor3D = STRUCT([floor1, floor2, floor3,floor4, floor5, floor6,floor7, floor8, floor9]);
+
+// piscine
+var bigPool3D = T([0,1])([1,1])(SIMPLEX_GRID([[20],[9],[0.8]]));
+var smallPool3D = T([0,1])([47,5])(SIMPLEX_GRID([[4],[11],[0.8]]));
+var pools3D = STRUCT([bigPool3D, smallPool3D]);
+
+//mura di marmo
+var wall1 = T([0,1,2])([1,0.75,0])(SIMPLEX_GRID([[7],[0.25],[4]]));
+var wall2 = T([0,1,2])([0.75,0.75,0])(SIMPLEX_GRID([[0.25],[21.50],[4]]));
+var wall3 = T([0,1,2])([1,22,0])(SIMPLEX_GRID([[8],[0.25],[4]]));
+var wall4 = T([0,1,2])([9,16.75,0])(SIMPLEX_GRID([[0.25],[5.50],[4]]));
+var wall5 = T([0,1,2])([7.5,15,1])(SIMPLEX_GRID([[19],[0.25],[3]]));
+var wall6 = T([0,1,2])([37.75,16,0])(SIMPLEX_GRID([[13.25],[0.25],[4]]));
+var wall7 = T([0,1,2])([51,4.75,0])(SIMPLEX_GRID([[0.25],[11.5],[4]]));
+var wall8 = T([0,1,2])([41.3,4.75,0])(SIMPLEX_GRID([[9.7],[0.25],[4]]));
+var wall9 = T([0,1,2])([25.2,7.2,1])(SIMPLEX_GRID([[9.6],[0.25],[3]]));
+var wall10 = T([0,1,2])([37.2,11.4,1])(SIMPLEX_GRID([[5.75],[0.25],[3]]));
+var walls3D = STRUCT([wall1, wall2, wall3, wall4, wall5, wall6, wall7, wall8, wall9, wall10]);
+
+// mura di vetro
+
+// bordo vetrata
+var glassWall1AxisX = SIMPLEX_GRID([[0.1],REPLICA(2)([3]),[0.05]]);
+var glassWall1AxisY = SIMPLEX_GRID([[0.1],REPLICA(2)([0.1,-2.7,0.1]),[3]]);
+var glass1 = SIMPLEX_GRID([[0.01],[-0.1,2.7,-0.2,2.65],[-0.05,3,-0.5]]);
+var glassWall1 =T([0,1,2])([31,7.5,1])(STRUCT([glassWall1AxisX,glassWall1AxisY,glass1]));
+var glassWall2 =T([0])([1])(glassWall1);
+
+var glassWall3AxisX = SIMPLEX_GRID([REPLICA(4)([0.1,-3.65]),[0.1],[2.8]]);
+var glassWall3AxisY = SIMPLEX_GRID([REPLICA(3)([-0.1,3.65]),[0.1],[0.05]]);
+var glass3 = SIMPLEX_GRID([REPLICA(3)([-0.1,3.65]),[0.01],[-0.05,2.7,0.05]]);
+var glassWall3= T([0,1,2])([30,5,1])(STRUCT([glassWall3AxisX,glassWall3AxisY,glass3]));
+
+var glassWall4AxisX = SIMPLEX_GRID([REPLICA(7)([0.1,-1.5]),[0.1],[2.8]]);
+var glassWall4AxisY = SIMPLEX_GRID([REPLICA(6)([-0.1,1.5]),[0.1],[0.05]]);
+var glass4 = SIMPLEX_GRID([REPLICA(6)([-0.1,1.5]),[0.01],[-0.05,2.7,0.05]]);
+var glassWall4 = T([0,1,2])([30,13.5,1])(STRUCT([glassWall4AxisX,glassWall4AxisY,glass4]));
+
+var glassWall5AxisX = SIMPLEX_GRID([[0.1],REPLICA(9)([0.1,-0.77]),[2.8]]);
+var glassWall5AxisY = SIMPLEX_GRID([[0.1],[7.25],[0.05,-2.7,0.05]]);
+var glass5 = SIMPLEX_GRID([[0.01],REPLICA(8)([-0.1,0.77]),[-0.05,2.7,0.05]]);
+var glassWall5 = T([0,1,2])([44.6,6.9,1])(STRUCT([glassWall5AxisX,glassWall5AxisY,glass5]));
+
+var glassWall6AxisX = SIMPLEX_GRID([REPLICA(4)([0.1,-1.85,0.05]),[0.1],[2.8]]);
+var glassWall6AxisY = SIMPLEX_GRID([REPLICA(4)([1.99]),[0.1],[0.05]]);
+var glass6 = SIMPLEX_GRID([REPLICA(4)([-0.1,1.89]),[0.01],[-0.05,2.7,0.05]]);
+var glassWall6 = T([0,1,2])([1,16.9,1])(STRUCT([glassWall6AxisX,glassWall6AxisY,glass6]));
+
+var glassWalls3D = STRUCT([glassWall1,glassWall2,glassWall3,glassWall4,glassWall5,glassWall6]);
+
+// scalinata
+var stair1 = SIMPLEX_GRID([[0.4],[3],[0.13]]); 
+var stair2 = T([0,2])([-0.4,0.13])(stair1);
+var stair3 = T([0,2])([-0.4,0.13])(stair2);
+var stair4 = T([0,2])([-0.4,0.13])(stair3);
+var stair5 = T([0,2])([-0.4,0.13])(stair4);
+var stair6 = T([0,2])([-0.4,0.13])(stair5);
+var stair7 = T([0,2])([-0.4,0.13])(stair6);
+var stairs3D = T([0,1])([38.4,1])(STRUCT([stair1,stair2,stair3,stair4,stair5,stair6,stair7]));
+
+// colonne
+var pillars3D = T([0,1])([26,6.8])(SIMPLEX_GRID([REPLICA(4)([0.15,-6.2]),
+	                         [0.15,-7,0.15],
+	                         [3,1]
+              ]));
+
+// panchina
+var benchLegs = T([0,1,2])([8, 14.2, 1])(SIMPLEX_GRID([REPLICA(8)([0.4,-1.7]),[0.4],[0.4]]));
+var benchSit = T([0,1,2])([7.9, 14.1, 1.38])(SIMPLEX_GRID([[15.3],[0.5],[0.1]]));
+var bench3D = STRUCT([benchSit,benchLegs]);
+
+//tetti
+var bigRoof = T([0,1,2])([24,4,4])(SIMPLEX_GRID([[23],[13],[0.25]]));
+var smallRoof = T([0,1,2])([0.5,13.2,4])(SIMPLEX_GRID([[9.5],[10],[0.25]]));
+var roofs3D= STRUCT([bigRoof,smallRoof]);
+
+//struttura completa
+var pavillionBarcelona = STRUCT([floor3D,pools3D, walls3D,glassWalls3D,stairs3D, pillars3D,bench3D,roofs3D]);
+DRAW(pavillionBarcelona);
